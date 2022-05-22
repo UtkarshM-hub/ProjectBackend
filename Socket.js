5e205d89e7e025861f013d0fb8007fc7ed73e710
Utkarsh Mandape <70505181+UtkarshM-hub@users.noreply.github.com>
2022-05-22 21:56:29 +0530

    updates

--- Socket.js
+++ Socket.js
@@ -4,7 +4,7 @@ module.exports={
     init:httpServer=>{
         io=require("socket.io")(httpServer,{
             cors:{
-                origin:"https://heroic-cupcake-dc4577.netlify.app"
+                origin:["https://heroic-cupcake-dc4577.netlify.app"]
             }
         });
         return io;
