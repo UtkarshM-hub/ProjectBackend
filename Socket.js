95bb66db2da2404ed0409d5846d891ba5a793e69
Utkarsh Mandape <70505181+UtkarshM-hub@users.noreply.github.com>
2022-05-22 21:46:36 +0530

    Origin changes

--- Socket.js
+++ Socket.js
@@ -4,7 +4,7 @@ module.exports={
     init:httpServer=>{
         io=require("socket.io")(httpServer,{
             cors:{
-                origin:"https://628886eca833125466a99c75--heroic-cupcake-dc4577.netlify.app"
+                origin:"https://heroic-cupcake-dc4577.netlify.app"
             }
         });
         return io;
